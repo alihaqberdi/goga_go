bc2f612b4f103a1e16b8a3aea8bd423b27235c53
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-11-17 11:30:00 +0500

    tender methods

--- internal/handler/tender.go
+++ internal/handler/tender.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"net/http"
+	"strconv"
 
 	"github.com/alihaqberdi/goga_go/internal/dtos"
 	. "github.com/alihaqberdi/goga_go/internal/handler/response"
@@ -29,57 +29,36 @@ func (h *Tender) CreateTender(c *gin.Context) {
 
 }
 
-func (h *Tender) CreateTenderHandler(c *gin.Context) {
-	var tenderDTO dtos.Tender
-
-	if err := c.ShouldBindJSON(&tenderDTO); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input: " + err.Error()})
+func (h *Tender) UpdateTender(c *gin.Context) {
+	data, err := bind[dtos.Tender](c)
+	if HasErr(c, err) {
 		return
 	}
 
-	createdTender, err := h.Service.Tenders.CreateTender(&tenderDTO)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create tender: " + err.Error()})
+	res, err := h.Service.Tenders.UpdateTender(data)
+
+	if HasErr(c, err) {
 		return
 	}
 
-	c.JSON(http.StatusCreated, createdTender)
+	Success(c, res, 201)
 }
 
-func (h *Tender) UpdateTenderHandler(c *gin.Context) {
-	var tenderDTO dtos.Tender
-
-	if err := c.ShouldBindJSON(&tenderDTO); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input: " + err.Error()})
+func (h *Tender) GetListTenders(c *gin.Context) {
+	limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
+	if HasErr(c, err) {
 		return
 	}
 
-	if err := h.Service.Tenders.UpdateTender(&tenderDTO); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to update tender: " + err.Error()})
+	offset, err := strconv.Atoi(c.DefaultQuery("offset", "0"))
+	if HasErr(c, err) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "tender updated successfully"})
+	res, err := h.Service.Tenders.Repo.Tenders.GetList(limit, offset)
+	if HasErr(c, err) {
+		return
+	}
+
+	Success(c, res, 201)
 }
-
-// func (h *Tender) GetListTendersHandler(c *gin.Context) {
-// 	limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
-// 	if err != nil || limit <= 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid limit"})
-// 		return
-// 	}
-
-// 	offset, err := strconv.Atoi(c.DefaultQuery("offset", "0"))
-// 	if err != nil || offset < 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid offset"})
-// 		return
-// 	}
-
-// 	tenders, err := h.Service.Tenders.Repo.Tenders.GetList(limit, offset)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to retrieve tenders: " + err.Error()})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, tenders)
-// }
