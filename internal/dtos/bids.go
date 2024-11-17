6eeb8e3d0e9a6b7a0d328be633a20fdf6e5df764
alihaqberdi <alhppypro@gmail.com>
2024-11-17 08:53:17 +0500

    feat: dtos bids

---
+++ internal/dtos/bids.go
@@ -0,0 +1,25 @@
+package dtos
+
+import (
+	"time"
+
+	"github.com/alihaqberdi/goga_go/internal/models/types"
+)
+
+type BidsBase struct {
+	TenderID     uint            `json:"tender_id" example:"1"`
+	ContractorID uint            `json:"contractor_id" example:"1"`
+	Price        float64         `json:"price" example:"100.00"`
+	DeliveryTime time.Time       `json:"delivery_time" example:"12"`
+	Comments     string          `json:"comments" example:"This is a comment"`
+	Status       types.BidStatus `json:"status" example:"pending"`
+}
+
+type BidsCreate struct {
+	BidsBase
+}
+
+type BidList struct {
+	BidsBase
+	ID uint `json:"id"`
+}
